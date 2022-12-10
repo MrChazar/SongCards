869875b926d46d5995be623eefca57239096ace1
MrChazar <jakubwiesniak102@gmail.com>
2022-12-11 00:03:31 +0100

    Adding Files

---
+++ FileHandle.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+
+public class FileHandle
+{
+    public class Lines
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public Lines(int a, string b)
+        {
+            Id = a;
+            Word = b;
+        }
+
+    }
+
+    public static void FileSave(List<string> data)
+    {
+        var Collection = new List<Lines>();
+        int numb = 1;
+        for (int i = 0; i < data.Count; i++)
+        {
+            if (data[i] == null || data[i] == "" || data[i] == "1" || data[i] == "2" || data[i].Length <= 3)
+            {
+                continue;
+            }
+            Collection.Add(new Lines(numb, data[i]));
+            numb++;
+        }
+        foreach (Lines a in Collection)
+        {
+            Console.WriteLine($"{a.Id} {a.Word}");
+
+        }
+        using (var writer = new StreamWriter("C:\\Users\\Asus\\source\\repos\\SongCards\\Cards\\Song.csv"))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(Collection);
+        }
+        Console.WriteLine("All done");
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using Genius;
+using System.Net;
+using HtmlAgilityPack;
+using System.Text;
+using System.Web;
+
+
+
+
+// Estabilishing Connection to api
+var client = new GeniusClient("a7Ur37IuSMicNb9pcqzbbwoUy49msqwPEyKndx8kBivPGRH17tpV4bteO410Rlp4");
+
+// Setting Encoding
+Console.OutputEncoding = Encoding.UTF8;
+
+// Variables for menu
+
+bool on = true;
+int action = 0;
+while (on)
+{
+    Console.Write("Welcome in SongCards, Choose your action:\n\t1: Get lyrics\n\t2: Quit\nAction:");
+    try
+    {
+        action = Convert.ToInt16(Console.ReadLine());
+    }
+    catch
+    {
+        continue;
+    }
+    switch (action)
+    {
+        case 1:
+            Console.Write("Give song name:");
+            string title = Console.ReadLine();
+            Console.Write("Give author name:");
+            string author = Console.ReadLine();
+            bool s = true;
+            int i = 1;
+            while (s)
+            {
+                var search = client.SearchClient.Search($"per_page=20&page={i}&q=" + title);
+                if (search.Result.Response.Hits.Count() == 0)
+                {
+                    Console.WriteLine("Not found");
+                    s = false;
+                }
+                // Console.WriteLine(search.Result.Response.Hits.Count());
+                var hits = search.Result.Response.Hits;
+                foreach (var item in hits)
+                {
+                    if (item.Result.PrimaryArtist.Name.ToUpper().Contains(author.ToUpper()))
+                    {
+                        Console.WriteLine(item.Result.FullTitle);
+                        Console.WriteLine(item.Result.Id);
+                        Console.WriteLine("Found");
+                        var song = await client.SongClient.GetSong(item.Result.Id);
+
+                        using (WebClient hclient = new WebClient())
+                        {
+                            HtmlWeb hw = new HtmlWeb();
+                            HtmlDocument doc = hw.Load(song.Response.Song.Url);
+                            var divs = doc.DocumentNode.SelectSingleNode("//div[contains(@class,'Lyrics__Container')]");
+                            var node = divs.InnerText;
+                            node = HttpUtility.HtmlDecode(node);
+                            Console.WriteLine(node);
+                            List<string> lista = Dict(node);
+                            FileHandle.FileSave(lista);
+
+                        }
+                        s = false;
+                    }
+                }
+                Console.WriteLine();
+                i += 1;
+            }
+            break;
+        case 2:
+            Console.WriteLine("Thanks For Using");
+            on = false;
+            break;
+        default:
+            Console.WriteLine("You made action that doesnt exists");
+            break;
+    }
+}
+
+
+
+
+
+
+
+List<string> Dict(string words)
+{
+    List<string> Result = new List<string>();
+    string temp = "";
+    foreach (char a in words)
+    {
+        if (a != ' ' && a != '[' && a != ']' && Char.IsUpper(a) != true)
+        {
+            temp += a;
+        }
+        else if (Char.IsUpper(a))
+        {
+            Result.Add(temp);
+            temp = "";
+            temp = Convert.ToString(Char.ToLower(a));
+        }
+        else
+        {
+            Result.Add(temp);
+            temp = "";
+        }
+    }
+    return Result;
+}
+
+
+
+
